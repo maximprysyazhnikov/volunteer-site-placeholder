6c3c81787ba19ef504434c2726180f8082da7efe
Антон <antonbliznuk71@gmail.com>
2026-01-13 12:31:34 +0000

    refactor with ruff.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,11 @@
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularSwaggerView,
     SpectacularRedocView,
+    SpectacularSwaggerView,
 )
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,7 +1,6 @@
 from drf_spectacular.utils import extend_schema
-from django.shortcuts import render
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 @extend_schema(
