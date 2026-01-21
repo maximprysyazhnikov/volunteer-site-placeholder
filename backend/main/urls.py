691248b2c985acdf5a527cf7eadb4c7539738c6f
Антон <antonbliznuk71@gmail.com>
2026-01-21 14:04:26 +0000

    Connected HelpCategory ViewSet in urls.py

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import ping
 
+from .views import HelpCategoryViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('help-category', HelpCategoryViewSet)
+
 urlpatterns = [
     path("ping/", ping),
+    path("", include(router.urls)),
 ]
