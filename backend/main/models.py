3aa6d6385f3a891d137be7a080ad40911d640fc3
Антон <antonbliznuk71@gmail.com>
2026-01-21 13:21:02 +0000

    Implement Help model.

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class HelpCategory(models.Model):
@@ -11,3 +12,59 @@ class HelpCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Help(models.Model):
+    class Kind(models.TextChoices):
+        REQUEST = "request", "Request"
+        OFFER = "offer", "Offer"
+
+    class Status(models.TextChoices):
+        NEW = "new", "New"
+        IN_PROGRESS = "in_progress", "In Progress"
+        DONE = "done", "Done"
+
+    title = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    kind = models.CharField(
+        max_length=20,
+        choices=Kind.choices,
+        default=Kind.REQUEST,
+    )
+    category = models.ForeignKey(HelpCategory, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.NEW,
+    )
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="created_by",
+    )
+    counterpart = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="counterpart_by",
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+    completed_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = "help"
+        verbose_name_plural = "helps"
+        ordering = ("created_at",)
+
+    def __str__(self):
+        return f"{self.kind}, {self.location}, from: {self.created_at}"
