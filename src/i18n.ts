256c273a97c3a8cdf5d96bd60c2af7fe33bf0a56
Roman Rusin <rgromkorusin12@gmail.com>
2026-01-23 21:20:13 +0200

    upgrade locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import en from "../public/locales/en/translation.json"
-import uk from "../public/locales/uk/translation.json";
+import en from "../src/locales/en/translation.json"
+import uk from "../src/locales/uk/translation.json";
 
 i18n.use(initReactI18next).init({
   resources: {
